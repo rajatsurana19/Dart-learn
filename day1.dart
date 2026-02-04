7c73519f5cf35be142150a02600ecb8d82edce60
Rajat Surana <rasurana19@gmail.com>
2026-02-04 20:42:35 +0530

    updated day1

---
+++ day1.dart
@@ -0,0 +1,22 @@
+void main(){
+    print("Hello");
+    //Strings
+    var a = "Hello";
+    String name = "Hi";
+
+    print(a+name);
+    //Integer
+    var x = 41;
+    int y = 24;
+
+    print(x);
+    print(y);
+
+    dynamic firstName = "Rajat";
+
+
+    const String f = "R";
+    final String k = "J";
+
+    
+}
